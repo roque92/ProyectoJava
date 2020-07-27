26e103db40b5607c8ba3f30172eb452c768b34b3
roque92 <leroque.asrc@gmail.com>
2020-07-27 17:56:15 -0600

    Update Class

--- proyecto/src/Modelo/Conector.java
+++ proyecto/src/Modelo/Conector.java
@@ -5,10 +5,36 @@
  */
 package Modelo;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
- * @author Jreyes
+ * @author JRoque
  */
 public class Conector {
+
+    private final String driver = "com.mysql.jdbc.Driver";
+    private final String server = "www.divisagt.com";
+    private final String user = "divisagt_cgm";
+    private final String password = "@Intecap2020";
+    private final String baseDatos ="divisagt_cgm";
+    private String cadena, error;
     
+    Connection con;
+    Statement st;
+    
+    //Metodo que habilita la conexion con la base de datos
+   public void connect(){
+       this.cadena = "jdbc:mysql://" +  this.server + "/" + this.baseDatos;
+       
+       try {
+           Class.forName(this.driver).newInstance();
+           this.con = (Connection) DriverManager.getConnection(this.cadena, this.user, this.password);
+       } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | SQLException e) {
+           System.err.println(e.getMessage());
+       }
+   }
 }

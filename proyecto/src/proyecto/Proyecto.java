b7c90f89d27286ad058760e1a18670e02c364bd1
roque92 <leroque.asrc@gmail.com>
2020-08-09 21:03:53 -0600

    Update Proyecto.java

--- proyecto/src/proyecto/Proyecto.java
+++ proyecto/src/proyecto/Proyecto.java
@@ -43,7 +43,8 @@ public class Proyecto {
         ControllerBuscarAsesor controllerBuscarAsesor = new ControllerBuscarAsesor(usuario, login, sbd, tbd, dvo);
         
         //login.setVisible(true);
-        usuario.setVisible(true);
+        //usuario.setVisible(true);
+        admin.setVisible(true);
     }
     
 }
